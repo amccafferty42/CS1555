17deed21e79ad3631f08a6380124b58a4d541295
TimSeaman <tms112@pitt.edu>
2018-11-16 15:26:10 -0500

    all triggers work

--- trigger.sql
+++ trigger.sql
@@ -1,1 +1,71 @@
-/* GROUP 16 TRIGGER.SQL*/
+
+create or replace trigger AutoProductID
+before insert
+on product
+for each row
+begin
+    select max(auction_id)+1 into :new.auction_id
+    from product;
+end;
+/
+
+create or replace trigger AutoBidNumber
+before insert
+on bidlog
+for each row
+begin
+    select max(bidsn)+1 into :new.bidsn
+    from bidlog;
+end;
+/
+
+create or replace trigger CreateCategory
+before insert
+on belongsto
+for each row
+declare
+    catCount number;
+begin
+    select count(:new.category) into catCount from category where name=:new.category;
+    if catCount = 0 then
+        insert into category values (:new.category, null);
+    end if;
+end;
+/
+
+create or replace trigger trig_bidTimeUpdate
+after insert
+on bidlog
+for each row
+begin
+    update ourSysDate
+    set c_date = c_date + (5/24/60/60);
+end;
+/
+
+
+create or replace trigger trig_updateHighBid
+after insert
+on bidlog
+for each row
+begin
+    update product
+    set amount = :new.amount
+    where auction_id = :new.auction_id;
+end;
+/
+
+create or replace trigger trig_closeAuctions
+after update
+on ourSysDate
+for each row
+begin
+    --check if auction should be closed and close it
+    update product
+    set status = 'close'
+    where :new.c_date > start_date + number_of_days;
+end;
+/
+
+
+
