7dd5afab929cbeccdc1be75bd22cd24650204f2b
TimSeaman <tms112@pitt.edu>
2018-11-16 15:12:57 -0500

    fixed all integrity constraint issues

--- insert.sql
+++ insert.sql
@@ -8,27 +8,27 @@ INSERT INTO Customer VALUES ('prucker_', '12345', 'Jean Prucker', '222 Chesterfi
 
 INSERT INTO Administrator VALUES ('admin', 'root', 'Barry McCockner ', '142 Oak St, Chicago IL 63302', 'barry@gmail.com');
 
-INSERT INTO Product VALUES (1, 'Ball', 'a baseball', 'tammy4life', '2018-05-06', 50, 1, 'under auction', null, null, null);
-INSERT INTO Product VALUES (2, 'Book', null, 'tammy4life', '2018-01-01', 12, 5, 'sold', 'prucker_', '2018-01-02', 20);
-INSERT INTO Product VALUES (3, 'Bat', 'a baseball bat', 'tammy4life', '2018-10-10', 5, 7, 'under auction', null, null, null);
-INSERT INTO Product VALUES (4, 'Broom', 'slightly swept', 'prucker_', '2018-11-03', 10, 9, 'sold', 'tammy4life', '2018-11-06', 20);
-INSERT INTO Product VALUES (5, 'Jet Ski', null, 'juice26', '2018-11-10', 17000, 20, 'under auction', null, null, null);
+INSERT INTO Product VALUES (1, 'Ball', 'a baseball', 'tammy4life', sysdate, 50, 1, 'under auction', null, null, null);
+INSERT INTO Product VALUES (2, 'Book', null, 'tammy4life', sysdate, 12, 5, 'close', 'prucker_', sysdate, 20);
+INSERT INTO Product VALUES (3, 'Bat', 'a baseball bat', 'tammy4life', sysdate, 5, 7, 'under auction', null, null, null);
+INSERT INTO Product VALUES (4, 'Broom', 'slightly swept', 'prucker_', sysdate, 10, 9, 'close', 'tammy4life', sysdate, 20);
+INSERT INTO Product VALUES (5, 'Jet Ski', null, 'juice26', sysdate, 17000, 20, 'under auction', null, null, null);
 
-INSERT INTO Bidlog VALUES (1, 1, 'juice26', '2018-05-06', 50);
-INSERT INTO Bidlog VALUES (2, 1, 'prucker_', '2018-05-06', 55);
-INSERT INTO Bidlog VALUES (3, 2, 'juice26', '2018-01-02', 12);
-INSERT INTO Bidlog VALUES (4, 2, 'prucker_', '2018-01-02', 13);
-INSERT INTO Bidlog VALUES (5, 2, 'juice26', '2018-01-02', 19);
-INSERT INTO Bidlog VALUES (6, 2, 'prucker_', '2018-01-02', 7);
-INSERT INTO Bidlog VALUES (7, 3, 'juice26', '2018-10-10', 20);
-INSERT INTO Bidlog VALUES (8, 4, 'juice26', '2018-11-03', 10);
-INSERT INTO Bidlog VALUES (9, 4, 'tammy4life', '2018-11-06', 12);
-INSERT INTO Bidlog VALUES (10, 4, 'juice26', '2018-11-06', 20);
+INSERT INTO Bidlog VALUES (1, 1, 'juice26', sysdate, 50);
+INSERT INTO Bidlog VALUES (2, 1, 'prucker_', sysdate, 55);
+INSERT INTO Bidlog VALUES (3, 2, 'juice26', sysdate, 12);
+INSERT INTO Bidlog VALUES (4, 2, 'prucker_', sysdate, 13);
+INSERT INTO Bidlog VALUES (5, 2, 'juice26', sysdate, 19);
+INSERT INTO Bidlog VALUES (6, 2, 'prucker_', sysdate, 7);
+INSERT INTO Bidlog VALUES (7, 3, 'juice26', sysdate, 20);
+INSERT INTO Bidlog VALUES (8, 4, 'juice26', sysdate, 10);
+INSERT INTO Bidlog VALUES (9, 4, 'tammy4life', sysdate, 12);
+INSERT INTO Bidlog VALUES (10, 4, 'juice26', sysdate, 20);
 
-INSERT INTO Category VALUES ('Equipment', 'Sports');
-INSERT INTO Category VALUES ('Motor Sports', 'Sports');
+INSERT INTO Category VALUES ('Equipment', null);
+INSERT INTO Category VALUES ('Motor Sports', null);
 INSERT INTO Category VALUES ('Sports', null);
-INSERT INTO Category VALUES ('Cleaning', 'Household Objects');
+INSERT INTO Category VALUES ('Cleaning', null);
 INSERT INTO Category VALUES ('Household Objects', null);
 
 INSERT INTO BelongsTo VALUES (1, 'Equipment');
