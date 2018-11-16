9ab10907fee46d3ec11ebec6991eb8fb2b463bd9
jonesdevin <daj59@pitt.edu>
2018-11-16 17:06:56 -0500

    Changed Address and Email fields && one func implemented

--- insert.sql
+++ insert.sql
@@ -2,17 +2,17 @@
 
 INSERT INTO ourSysDATE VALUES (sysdate);
 
-INSERT INTO Customer VALUES ('tammy4life', 'starwars', 'Tammy Jones', '420 Dirt St, Birmingham AL 21520', 'taminatorxx@yahoo.com');
-INSERT INTO Customer VALUES ('juice26', 'playhard', 'LeVeon Bell', '8 Sunny Breeze Ln, Miami FL 12836', 'juice@nfl.com');
-INSERT INTO Customer VALUES ('prucker_', '12345', 'Jean Prucker', '222 Chesterfield Rd, Pittsburgh PA 15213', 'jprucker@aol.com');
+INSERT INTO Customer VALUES ('tammy4life', 'starwars', 'Tammy Jones', '420 D St, Bham AL 21', 'taminator@yahoo.com');
+INSERT INTO Customer VALUES ('juice26', 'playhard', 'LeVeon Bell', '8 B Ln, Miami FL 1', 'juice@nfl.com');
+INSERT INTO Customer VALUES ('prucker_', '12345', 'Jean Prucker', '22 C Rd, Pburgh PA 5', 'jprucker@aol.com');
 
-INSERT INTO Administrator VALUES ('admin', 'root', 'Barry McCockner ', '142 Oak St, Chicago IL 63302', 'barry@gmail.com');
+INSERT INTO Administrator VALUES ('admin', 'root', 'Barry McCockner ', '1 O St, Chicago IL 6', 'barry@gmail.com');
 
 INSERT INTO Product VALUES (1, 'Ball', 'a baseball', 'tammy4life', sysdate, 50, 1, 'under auction', null, null, null);
 INSERT INTO Product VALUES (2, 'Book', null, 'tammy4life', sysdate, 12, 5, 'close', 'prucker_', sysdate, 20);
 INSERT INTO Product VALUES (3, 'Bat', 'a baseball bat', 'tammy4life', sysdate, 5, 7, 'under auction', null, null, null);
 INSERT INTO Product VALUES (4, 'Broom', 'slightly swept', 'prucker_', sysdate, 10, 9, 'close', 'tammy4life', sysdate, 20);
-INSERT INTO Product VALUES (5, 'Jet Ski', null, 'juice26', sysdate, 17000, 20, 'under auction', null, null, null);
+INSERT INTO Product VALUES (5, 'Jet Ski', null, 'juice26', sysdate, 17000, 20, 'in auction', null, sysdate, null);
 
 INSERT INTO Bidlog VALUES (1, 1, 'juice26', sysdate, 50);
 INSERT INTO Bidlog VALUES (2, 1, 'prucker_', sysdate, 55);
@@ -36,3 +36,4 @@ INSERT INTO BelongsTo VALUES (2, 'Household Objects');
 INSERT INTO BelongsTo VALUES (3, 'Equipment');
 INSERT INTO BelongsTo VALUES (4, 'Cleaning');
 INSERT INTO BelongsTo VALUES (5, 'Motor Sports');
+
