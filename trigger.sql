c77f7d407509c774aca669eb1e7a25e1e54ac7dc
TimSeaman <tms112@pitt.edu>
2018-11-19 13:25:31 -0500

    Added procedures

--- trigger.sql
+++ trigger.sql
@@ -1,14 +1,3 @@
-
-create or replace trigger AutoProductID
-before insert
-on product
-for each row
-begin
-    select max(auction_id)+1 into :new.auction_id
-    from product;
-end;
-/
-
 create or replace trigger AutoBidNumber
 before insert
 on bidlog
@@ -16,20 +5,6 @@ for each row
 begin
     select max(bidsn)+1 into :new.bidsn
     from bidlog;
-end;
-/
-
-create or replace trigger CreateCategory
-before insert
-on belongsto
-for each row
-declare
-    catCount number;
-begin
-    select count(:new.category) into catCount from category where name=:new.category;
-    if catCount = 0 then
-        insert into category values (:new.category, null);
-    end if;
 end;
 /
 
@@ -60,7 +35,6 @@ after update
 on ourSysDate
 for each row
 begin
-    --check if auction should be closed and close it
     update product
     set status = 'close'
     where :new.c_date > start_date + number_of_days;
@@ -85,8 +59,6 @@ begin
     return a_count;
 end;
 /
-
-
 
 CREATE OR REPLACE FUNCTION fucn_bidCount(x in integer, u in varchar2)return integer
 IS
@@ -121,3 +93,30 @@ begin
     return a_sum;
 end;
 /
+
+create or replace procedure proc_putProduct (prod_name in varchar2, cat_name in varchar2, num_days in integer, des in varchar2)
+as
+    new_auction_id integer;
+    curr_date date;
+    dummy varchar2(20);
+begin
+    select c_date into curr_date from ourSysDate;
+    select max(auction_id)+1 into new_auction_id from product;
+    
+    insert into product(auction_id, name, description, start_date, number_of_days, status)
+    values(new_auction_id, prod_name, des, curr_date, num_days, 'under auction');
+    
+    select name into dummy
+    from category
+    where name = cat_name;
+exception
+    when no_data_found then
+    insert into category(name) values(cat_name);
+end;
+/
+/*
+select auction_id, name from product order by auction_id;
+select * from category;
+
+call proc_putProduct('skatasdes', 'asdfasdf', 1, 'loud');
+
