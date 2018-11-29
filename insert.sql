83d55caaae2f5aa1487bb2ca59ebbd46d0ede041
jonesdevin <daj59@pitt.edu>
2018-11-28 22:41:16 -0500

    Common Issues 2,3,4,5,7 FIXED --- Edited insert.sql to test and show IC,func,proc work as expected

--- insert.sql
+++ insert.sql
@@ -1,7 +1,7 @@
 /* GROUP 16 INSERT.SQL*/
 
 --inserting everything
-INSERT INTO ourSysDATE VALUES (sysdate);
+INSERT INTO ourSysDATE VALUES (to_date('01.01/2000 00:00:00' ,'mm.dd/yyyy hh24:mi:ss'));
 
 INSERT INTO Customer VALUES ('tammy4life', 'starwars', 'Tammy Jones', '420 D St, Bham AL 21', 'taminator@yahoo.com');
 INSERT INTO Customer VALUES ('juice26', 'playhard', 'LeVeon Bell', '8 B Ln, Miami FL 1', 'juice@nfl.com');
@@ -9,11 +9,11 @@ INSERT INTO Customer VALUES ('prucker_', '12345', 'Jean Prucker', '22 C Rd, Pbur
 
 INSERT INTO Administrator VALUES ('admin', 'root', 'Barry McCockner ', '1 O St, Chicago IL 6', 'barry@gmail.com');
 
-INSERT INTO Product VALUES (1, 'Ball', 'a baseball', 'tammy4life', sysdate, 50, 1, 'under auction', null, null, null);
-INSERT INTO Product VALUES (2, 'Book', null, 'tammy4life', sysdate, 12, 5, 'under auction', null, null, null);
-INSERT INTO Product VALUES (3, 'Bat', 'a baseball bat', 'tammy4life', sysdate, 5, 7, 'under auction', null, null, null);
-INSERT INTO Product VALUES (4, 'Broom', 'slightly swept', 'prucker_', sysdate, 10, 9, 'under auction', null, null, null);
-INSERT INTO Product VALUES (5, 'Jet Ski', null, 'juice26', sysdate, 17000, 20, 'under auction', null, null, null);
+INSERT INTO Product VALUES (1, 'Ball', 'a baseball', 'tammy4life', getCurDate, 50, 1, 'under auction', null, initSellDate(1), null);
+INSERT INTO Product VALUES (2, 'Book', null, 'tammy4life', getCurDate, 12, 5, 'under auction', null, initSellDate(5), null);
+INSERT INTO Product VALUES (3, 'Bat', 'a baseball bat', 'tammy4life', getCurDate, 5, 7, 'under auction', null, initSellDate(7), null);
+INSERT INTO Product VALUES (4, 'Broom', 'slightly swept', 'prucker_', getCurDate, 10, 9, 'under auction', null, initSellDate(9), null);
+INSERT INTO Product VALUES (5, 'Jet Ski', null, 'juice26', getCurDate, 17000, 20, 'under auction', null, initSellDate(20), null);
 
 INSERT INTO Bidlog VALUES (1, 1, 'juice26', sysdate, 50);
 INSERT INTO Bidlog VALUES (2, 1, 'prucker_', sysdate, 55);
@@ -30,9 +30,8 @@ INSERT INTO Bidlog VALUES (10, 4, 'juice26', sysdate, 20);
 INSERT INTO Category VALUES ('Equipment', null);
 INSERT INTO Category VALUES ('Motor Sports', null);
 INSERT INTO Category VALUES ('Sports', null);
-INSERT INTO Category VALUES ('Cleaning', null);
 INSERT INTO Category VALUES ('Household Objects', null);
-
+INSERT INTO Category VALUES ('Cleaning', 'Household Objects');
 
 
 INSERT INTO BelongsTo VALUES (1, 'Equipment');
@@ -59,6 +58,11 @@ INSERT INTO Bidlog VALUES (11, 1, 'juice26', sysdate, 54);
 select func_productCount(6, 'Motor Sports') from dual;
 select func_bidCount(6, 'prucker_') from dual;
 select func_buyingAmount(6, 'juic26') from dual;
-select * from Product;
+
 select * from Bidlog;
+select to_char(getCurDate, 'mm.dd/yyyy hh24:mi:ss') from dual;
+
+CALL proc_putProduct('juice26', 'turtle', 'Equipment,Tree,Household Objects,APE,ANIMAL,FUCK,ME,HARD', 5, 'SUPER SLOW', 10);
+
+select * from product;
 
